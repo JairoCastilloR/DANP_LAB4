2238b61f76f1f0b6622bf94b9e938881f437ca3e
Jairo Castillo <jcastillo@unsa.edu.pe>
2023-06-01 22:58:36 -0500

    Agregue model, BookDao, StudentDao y UnitDao, ademas cree database con sus respectivas entidades y un repositorio, tambien agregue error de room xD

---
+++ app/src/main/java/com/example/danp_lab4/model/AppDatabase.kt
@@ -0,0 +1,45 @@
+package com.example.danp2023room.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.danp2023room.entities.BookEntity
+import com.example.danp2023room.entities.StudentEntity
+import com.example.danp2023room.entities.UnitEntity
+import com.example.danp_lab04.model.UnitDao
+
+
+@Database(
+    entities = [StudentEntity::class, BookEntity::class, UnitEntity::class],
+    version = 5
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun unitDao(): UnitDao
+    abstract fun studentDao(): StudentDao
+    abstract fun bookDao(): BookDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "database-name"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+
+                }
+
+                return instance
+            }
+        }
+    }
+}
